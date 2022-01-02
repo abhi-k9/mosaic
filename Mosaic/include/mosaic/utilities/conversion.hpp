45e6c3483c7f449555ed37208d8581dbb3aeb9db
k09 <kale3930@gmail.com>
2022-01-02 02:20:43 -0500

    Adopt new C++11 conversion detection strategy

--- Mosaic/include/mosaic/utilities/conversion.hpp
+++ Mosaic/include/mosaic/utilities/conversion.hpp
@@ -67,29 +67,43 @@ public:
     }
 
     #define SUPERSUBCLASS(T, U) \
-        @"C++20 compiler detected, please use `SUPERSUBCLASS` function instead of the macro. `Usage: SUPERCLASS<T, U>()`.
+        @"C++20 conforming compiler detected, please use `SUPERSUBCLASS` function instead of the macro. `Usage: SUPERCLASS<T, U>()`.
 
     #define SUPERSUBCLASS_STRICT(T, U) \
-        @"C++20 compiler detected, please use `SUPERSUBCLASS` function instead of the macro. Usage: `SUPERCLASS_STRICT<T, U>()`.
+        @"C++20 conforming compiler detected, please use `SUPERSUBCLASS` function instead of the macro. Usage: `SUPERCLASS_STRICT<T, U>()`.
 
-/* DO NOT USE : `constexpr` not guaranteed to be evaluated at compile time */
-// #elif __cplusplus >= 201103L // Compiler supports C++11 standard and above.
+/* Note: 
+ *      `constexpr` NOT guaranteed to be evaluated at compile time at all places.
+ */
+#elif __cplusplus >= 201103L // Compiler supports C++11 standard and above.
 
-//     template <bool value>
-//     struct Bool{
-//         constexpr operator bool () {return true;}
-//     };
+    template <class T, class U>
+    constexpr bool SUPERSUBCLASS() {
+        
+        using cpT = const T *;
+        using cpU = const U *;
+        using cpv = const void *;
 
-//     template <>
-//     struct Bool<false>{
-//         constexpr operator bool () {return false;}
-//     };
+        return (Conversion<cpU, cpT>::exists && !Conversion<cpT, cpv>::sameType);
+    }
 
-//     template <class T, class U>
-//     using SUPERSUBCLASS = Bool<Conversion<const U *, const T *>::exists && !Conversion<const T *, const void *>::sameType>;
+    template <class T, class U>
+    constexpr bool SUPERSUBCLASS_STRICT() {
+        
+        using cpT = const T *;
+        using cpU = const U *;
 
-//     template <class T, class U>
-//     using SUPERSUBCLASS_STRICT = Bool<SUPERSUBCLASS<T, U>() && !Conversion<const T, const U>::sameType>;
+        using cT = const T;
+        using cU = const U;
+
+        return (SUPERSUBCLASS<T, U>() && !Conversion<cT, cU>::sameType);
+    }
+
+    #define SUPERSUBCLASS(T, U) \
+        @"C++11 conforming compiler detected, please use `SUPERSUBCLASS` function instead of the macro. `Usage: SUPERCLASS<T, U>()`.
+
+    #define SUPERSUBCLASS_STRICT(T, U) \
+        @"C++11 conforming compiler detected, please use `SUPERSUBCLASS` function instead of the macro. Usage: `SUPERCLASS_STRICT<T, U>()`.
 
 #else
 
