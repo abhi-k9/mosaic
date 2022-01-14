67f905d998f6621b35a226f2e27f3c9b2e5acdab
k09 <kale3930@gmail.com>
2022-01-14 01:27:21 -0500

    Add compute length operation for `ParameterPack`

--- Mosaic/include/mosaic/utilities/parameter_pack.hpp
+++ Mosaic/include/mosaic/utilities/parameter_pack.hpp
@@ -9,8 +9,12 @@
 
 namespace mosaic{
 
+// Forward declaration.
 template <class, class ...> struct Split;
 
+
+/*! @brief Structure to store a type parameter pack.
+ */ 
 template <class ...Types>
 struct ParameterPack {
     using HTSplit = Split<Types...>;
@@ -18,10 +22,27 @@ struct ParameterPack {
     using Tail = typename HTSplit::Tail;
 };
 
+
+/*! @brief Split parameter pack into head (first type in the pack) 
+ *  and tail (rest of the types).
+ */ 
 template <class H, class ...T>
 struct Split {
     using Head = H;
     using Tail = ParameterPack<T...>;
+};
+
+
+/*! @brief Compute the length of given `ParameterPack`.
+ */
+template <typename... Pack>
+struct Length {
+    enum { value = sizeof...(Pack) };
+};
+
+template <typename... Pack>
+struct Length <ParameterPack<Pack...>> {
+    enum { value = sizeof...(Pack) };
 };
 
 } // end namespace `mosaic`
