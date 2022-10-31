28154026a6be85d973ab414644983c02d147911d
k09 <kale3930@gmail.com>
2022-10-30 22:03:52 -0400

    Add `AlwaysFalse` and `AlwaysTrue` markers

--- Mosaic/include/mosaic/utilities/markers.hpp
+++ Mosaic/include/mosaic/utilities/markers.hpp
@@ -5,6 +5,8 @@
  *  @details Defines: 
  *  - `NullType` class that serves as a null marker for types.
  *  - `EmptyType` struct that can be used as a default filler type for template.
+ *  - `AlwaysFalse` template struct that provides `false` value on any instantiation.
+ *  - `AlwaysTrue` template struct that provides `true` value on any instantiation.
  */
 
 namespace mosaic {
@@ -12,5 +14,20 @@ namespace mosaic {
     class NullType {};
 
     struct EmptyType {};
+
+
+    /*! @brief Template that provides `false` value on any instantiation.
+     */
+    template <typename T>
+    struct AlwaysFalse {
+        enum { value = false };
+    };
+
+    /*! @brief Template that provides `true` value on any instantiation.
+     */
+    template <typename T>
+    struct AlwaysTrue {
+        enum { value = true };
+    };
 
 } // end namespace `mosaic`
